8d1450fa7742801d3657f1353e7418fb1731f4a1
Sansone11 <sansonemirco@gmail.com>
2022-09-23 16:09:55 +0200

    include-bug

--- prima_versione/index.php
+++ prima_versione/index.php
@@ -1,4 +1,7 @@
 <!-- Prima Versione: Stampiamo i dischi solo con l’utilizzo di PHP, che stampa direttamente i dischi in pagina: al caricamento della pagina ci saranno tutti i dischi. -->
 <?php 
+include_once__dir
+header('Content-Type:application/json')
+albums=[]
 
 ?>
